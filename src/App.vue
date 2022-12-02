2f20c768cc4b3c62274ba6625d4d886466f60606
lollobianco <loreb2002@gmail.com>
2022-12-02 13:29:41 +0100

    Importati file e creata immagine jumbo

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    
+    <HeaderSection/>
+
+  </div>
+</template>
+
+<script>
+import HeaderSection from './components/HeaderSection.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderSection
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    
+    <div id="header-background">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSection',
+  props: {
+    
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+#header-background{
+  background-image: url(../assets/home-banner.jpg);
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 100%;
+  height: 1079px;
+}
+</style>
